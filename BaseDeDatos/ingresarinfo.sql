9ababa778ba707b850e99306a587bc75b4e3609c
marcelo8dog <marcelo8dog@hotmail.com>
2017-05-23 01:23:43 -0500

    base de datos corregida y modificada. Con sql para ingresar informacion

---
+++ BaseDeDatos/ingresarinfo.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuario(id_usuario,nombre_usuario) VALUES ("robérto64","Roberto");
+INSERT INTO usuario(id_usuario,nombre_usuario) VALUES ("aaron64","Aarón");
+INSERT INTO usuario(id_usuario,nombre_usuario) VALUES ("david64","David");
+INSERT INTO usuario(id_usuario,nombre_usuario) VALUES ("fernanda64","Fernanda");
+INSERT INTO usuario(id_usuario,nombre_usuario) VALUES ("kevin64@}]+?¿¡!#$","Kevín");
+INSERT INTO usuario(id_usuario,nombre_usuario) VALUES ("angy64","Angélica");
