55a10b90656b3c8dff33d1db4a2e321ad3b63dd0
aarongn22 <aaron_shark_gn@outlook.es>
2017-05-23 13:12:36 -0500

    Registro

--- Templates/registro.php
+++ Templates/registro.php
@@ -1,7 +1,9 @@
 <?php
-	if (isset($_POST["cuentanueva"]) && isset($_POST["contranueva"])){
+
+	if(isset($_POST["cuentanueva"]) && isset($_POST["contranueva"])&& isset($_POST["nomnuevo"]) && isset($_POST["apellidonuevo"]) && isset($_POST["fechnuevo"]) && isset($_POST["correo"]))
+	{
 		//Conexión a la base de datos
-		$conexion = mysqli_connect("localhost", "root", "", "usuarios");
+		$conexion = mysqli_connect("localhost","root","","final");
 		if (mysqli_connect_errno($conexion)) {
 			echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
 		}
@@ -9,15 +11,19 @@
 		else{
 			$cuenta = mysqli_real_escape_string ($conexion, $_POST["cuentanueva"]);
 			$contra = mysqli_real_escape_string ($conexion, $_POST["contranueva"]);
-			$oregano = md5(uniqid(rand(), true));
+			$nombre = mysqli_real_escape_string ($conexion, $_POST["nomnuevo"]);
+			$apellido = mysqli_real_escape_string ($conexion, $_POST["apellidonuevo"]);
+			$nacimiento = mysqli_real_escape_string ($conexion, $_POST["fechnuevo"]);
+			$correo = mysqli_real_escape_string ($conexion, $_POST["correo"]);
+			$oregano = md5(uniqid(rand(), TRUE));
 			$mix = hash('sha512', $oregano.$contra);
 			unset($contra);
-			$resultado = mysqli_query($conexion, "INSERT INTO usuario(nombre_usuario,password,oregano) VALUES('".$cuenta."','".$mix."','".$oregano."');");
+			$resultado = mysqli_query($conexion, "INSERT INTO usuario(nombre_usuario,apellido_usuario,id_usuario,password,oregano,nacimiento_usuario,correo_usuario) VALUES('".$nombre."','".$apellido."','".$cuenta."','".$mix."','".$oregano."','".$nacimiento."','".$correo."');");
 			echo "USUARIO REGISTRADO CON EXITO";
 		}
 		mysqli_close($conexion);
 	}
-	else{
+	else
 		echo "No esta correctamente completado el formulario de registro.";
-	}
+	
 ?>
