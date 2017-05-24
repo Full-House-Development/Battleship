0e8cdf1e052c2cc5e73555ea9aee1202384a5e7b
Karla Denia Salas Jiménez <Karla Denia Salas Jimnez>
2017-05-24 14:39:25 -0500

    juego

--- Templates/juego/index.php
+++ Templates/juego/index.php
@@ -2,30 +2,62 @@
 <html>
   <head>
     <meta charset="utf-8"/>
+    <script type="text/javascript" src="../../Documents/js/jquery-3.2.1.js"></script>
+
     <title>BattleShip</title>
   </head>
   <body>
-      <table id="mesa">
+
+      <table id="suMesa" style="position: absolute; top: 8px; padding-left: 40em;">
 
       </table>
-  </body>
+            <table id="miMesa" style="position: absolute; top: 8px; padding-left: 2em;">
+
+      </table>
+    <script type="text/javascript">
+    function dispara(donde,  x, y) {
+      var id='#'+donde+y+x;
+      if (donde =='m'){ //mio
+        $(id).empty();
+         $(id).append($('<img srcset="../../Resources/Images/pulsado.svg"/>'));
+      }
+      else if (donde=='s'){//su
+         id='#'+donde+x+y;
+        $(id).empty();
+         $(id).append($('<img srcset="../../Resources/Images/pulsado.svg"/>'));
+      }
+      els
+      return 0;
+    }
+    for (var alfa = 0; alfa < 10; alfa++) {
+        renglon =$('<tr></tr>');
+        renglon.attr('id', 'm'+alfa);
+        $('#miMesa').append(renglon);
+        renglon =$('<tr></tr>');
+        renglon.attr('id', 's'+alfa);
+        $('#suMesa').append(renglon);
+        for (var beta = 0; beta < 10; beta++) {
+            columna =$('<td></td>');
+            columna.append($('<img srcset="../../Resources/Images/normal.svg"/>'));
+            columna.attr('id', 'm'+alfa+beta);
+            columna.attr('class', 'miMesa');
+            $('#m'+alfa).append(columna);
+            $('#m'+alfa+beta).on('click', function(e){
+              id=this.id;
+              dispara(id[0],id[2],id[1] );
+            });
+            columna =$('<td></td>');
+            columna.append($('<img srcset="../../Resources/Images/normal.svg"/>'));
+            columna.attr('id', 's'+alfa+beta);
+            columna.attr('class', 'miMesa');
+            $('#s'+alfa).append(columna);
+            $('#s'+alfa+beta).on('click', function(e){
+              id=this.id;
+              dispara(id[0],id[1],id[2] );
+            });
+        }
+    }
+    </script>
+
+</body>
 </html>
-
-<?php
-$mapaEnemigo= array();
-$mapaUsuario= array();
-for ($alfa=0; $alfa <10 ; $alfa++) {
-  echo "<script type='text/javascript'>$('#mesa').append($('<tr><td>Hola</td></tr>'))</script>";
-  for ($beta=0; $beta <10 ; $beta++) {
-    $mapaUsuario[$alfa][$beta]=1;
-    $mapaEnemigo[$alfa][$beta]=1;
-  }
-}
-?>
-
-
-
- <?php
- echo $mapaUsuario[3][3];
-
- ?>
