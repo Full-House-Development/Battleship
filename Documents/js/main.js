c5e6d8793e586d251dc141b6295af542d72b2ecc
marcelo8dog <marcelo8dog@hotmail.com>
2017-05-27 01:47:51 -0500

    cambio2

---
+++ Documents/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+$(document).ready(function(){
+      $('.slider').slider();
+    });
+$(document).ready(function(){
+    $('.modal').modal();
+	});
+$(document).ready(function(){
+			$('.carousel').carousel();
+	});
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+	});
+$(document).ready(function() {
+    $('select').material_select();
+	});
