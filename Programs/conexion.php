c4ca96418ce4bc323839c0b819f9b0022345d358
marcelo8dog <marcelo8dog@hotmail.com>
2017-05-27 01:43:56 -0500

    cambios

--- Programs/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-	function conectar(){
-	 $conexion = mysqli_connect("132.248.96.53","battleship","sugardadies","final");
-	 return $conexion;
-	 }
-?>
--- Templates/conection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function conection()
-	{
-		$cone=mysqli_connect("132.248.96.53","battleship","sugardadies","final");
-		mysqli_set_charset($cone,"utf8");
-		return $cone;
-	}
-?>
